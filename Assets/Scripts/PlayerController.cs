522b2b2ba6fa09abe877c6701e0cb7ef0bd7cec8
WootenWarrior <152801888+WootenWarrior@users.noreply.github.com>
2024-02-15 17:26:53 +0000

    Initial Github Setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,24 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveSpeed;
-    public Vector2 force;
-    public float dampingFactor;
-    public Vector2 placeholderForce;
+    public float movespeed = 5f;
+    Rigidbody2D playerRb;
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Vector2 PlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-        rb.velocity = PlayerInput * moveSpeed;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision) {
-        Debug.Log(collision);
-        if(collision.collider.CompareTag("TestCollision")){
-            force += placeholderForce;
-            Destroy(collision.gameObject);
-        }
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+        Vector3 moveVector = new Vector3(Horizontal*movespeed,Vertical*movespeed,0);
+        playerRb.velocity = moveVector;
     }
 }
